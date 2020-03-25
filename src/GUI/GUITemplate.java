4dbab730c244394366c270e23a728bebc5acae9c
Yuxuan Wu <sodawyx@126.com>
2020-03-25 21:18:33 +0800

    Initializing

---
+++ src/GUI/GUITemplate.java
@@ -0,0 +1,24 @@
+package GUI;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class GUITemplate extends JFrame implements ActionListener{
+    JFrame basicFrame = new JFrame(" ");
+    JPanel basicPanel;
+
+    public GUITemplate(){
+        basicPanel = new JPanel();
+        basicFrame.add(basicPanel);
+        basicFrame.setSize(800, 600);
+        basicFrame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        GUITemplate guiTemplate = new GUITemplate();
+    }
+}
