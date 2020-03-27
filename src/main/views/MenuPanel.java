c9bff94b311453f57f805f2f047c2efc5de9c028
williamkai429 <williamkai429@gmail.com>
2020-03-28 01:53:46 +0800

    更新Menu的topPanel

--- src/main/views/MenuPanel.java
+++ src/main/views/MenuPanel.java
@@ -5,11 +5,12 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class MenuPanel extends JPanel {
-    private double fixedPrice = 9.99;      //the price of fixed dishes, default 9.99 pounds
-    private double extraNoriPrice = 1;     //the price of extra Nori, default 1 pounds
-    private double extraBoilEggPrice = 1;  //the price of extra boiled eggs, default 1 pounds
-    private double extraBamShootPrice = 1; //the price of extra banboo shoot, default 1 pounds
-    private double extraChashuPrice = 2;   //the price of extra Chashu, default 2 pounds
+    //variables
+    private double fixedPrice ;      //the price of fixed dishes, default 9.99 pounds
+    private double extraNoriPrice ;     //the price of extra Nori, default 1 pounds
+    private double extraBoilEggPrice ;  //the price of extra boiled eggs, default 1 pounds
+    private double extraBamShootPrice ; //the price of extra banboo shoot, default 1 pounds
+    private double extraChashuPrice ;   //the price of extra Chashu, default 2 pounds
     private String soupType;
     private String noodleType;
     private String sprOnionType;
@@ -21,35 +22,65 @@ public class MenuPanel extends JPanel {
     private int extraBoilEggNum;
     private int extraBamshootNum;
     private int extraChashuNum;
+
+    //MenuPanel
     private JPanel topPanel;
     private JPanel midPanel;
     private JPanel botPanel;
+    private JCheckBox ramen;
 
     public MenuPanel() {
         init();
     }
 
     public void init() {
+        //初始化传递参数，用于测试
+        fixedPrice = 9.99;
         //Initialize the sub-panel of this panel
         topPanel = new JPanel();
         midPanel = new JPanel();
         botPanel = new JPanel();
         //Set the topPanel
+        GridBagConstraints c1 = new GridBagConstraints();
+        topPanel.setLayout(new GridBagLayout());
         topPanel.setBorder(new TitledBorder(null, "top ", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, Color.BLACK));
-        /**
-         * code here is the design of topPanel
-         */
+        JCheckBox ramen = new JCheckBox("Ramen ￡"+fixedPrice,true);
+        Font ramenFont = new Font("Times New Roman",Font.BOLD+ Font.ITALIC,20);
+        ramen.setFont(ramenFont);
+        ramen.setEnabled(false);
+        JLabel title = new JLabel("<html><body>Totoro Ramen<br>Designed by Mr.Miyazaki<body></html>");
+        Font titleFont = new Font("Times New Roman",Font.BOLD+ Font.ITALIC,25);
+        title.setFont(titleFont);
+        c1.anchor = GridBagConstraints.WEST;
+        c1.gridx = 0;
+        c1.gridy = 0;
+        c1.gridwidth = 1;
+        c1.gridheight = 1;
+        c1.weightx = 0.3;
+        c1.weighty = 1;
+        topPanel.add(ramen,c1);
+        c1.gridx = 1;
+        c1.weightx = 0.7;
+        topPanel.add(title,c1);
+
+
         //set the midPanel
         midPanel.setBorder(new TitledBorder(null, "mid ", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, Color.BLACK));
         /**
          * code here is the design of midpanel
          */
+
+
         //set the bottom Panel
         botPanel.setBorder(new TitledBorder(null, "bottom ", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, Color.BLACK));
         /**
          * code here is the design of bottompanel
          */
+
+
         //Set the layout and initialize this panel
+
+
         //Set layout of this panel
         this.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
@@ -59,10 +90,10 @@ public class MenuPanel extends JPanel {
         c.gridwidth = 1;
         c.gridheight = 1;
         c.weightx = 1;
-        c.weighty = 0.15;
+        c.weighty = 0.03;
         this.add(topPanel,c);
         c.gridy = 1;
-        c.weighty = 0.85;
+        c.weighty = 0.87;
         this.add(midPanel,c);
         c.gridy = 2;
         c.weighty = 0.1;
