5e8e9e9bb9da7e5e1423a3496a81cce118d883a8
1259704385 <xiurufeng@xiurufengdeMacBook-Pro.local>
2020-03-27 13:39:38 +0800

    Merge branch 'master' of /Users/xiurufeng/IdeaProjects/SEProject with conflicts.

---
+++ src/main/views/TicketPlane.java
@@ -0,0 +1,17 @@
+package main.views;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicketPlane extends JFrame implements ActionListener {
+     JFrame TicketJFrame = new JFrame("Ticket for your order" );
+     GridBagLayout gridBag = new GridBagLayout();     //Layout manager
+     GridBagConstraints c = null;                     //Constraint
+
+
+     @Override
+     public void actionPerformed(ActionEvent actionEvent) {
+
+     }
+}
