ace7ce01f2f073cdc4d747095b5cac743a27b152
wjzyn <852266201@qq.com>
2020-03-30 22:06:03 +0800

    Menu 的TDD

--- test/main/entity/MenuTest.java
+++ test/main/entity/MenuTest.java
@@ -5,57 +5,60 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MenuTest {
-
+    Menu n=new Menu();
     @Test
     void getFixedPrice() {
-        Menu n=new Menu();
         assertEquals(9.99,n.getFixedPrice());
     }
 
     @Test
     void setFixedPrice() {
-        Menu n=new Menu();
         n.setFixedPrice(5.5);
         assertEquals(5.5,n.getFixedPrice());
     }
 
-    @Test
-    void testGetFixedPrice() {
-    }
-
-    @Test
-    void testSetFixedPrice() {
-    }
 
     @Test
     void getExtraNoriPrice() {
+        assertEquals(1,n.getExtraNoriPrice());
     }
 
     @Test
     void setExtraNoriPrice() {
+        n.setExtraNoriPrice(22.2);
+        assertEquals(22.2,n.getExtraNoriPrice());
     }
 
     @Test
     void getExtraBoilEggPrice() {
+        assertEquals(1,n.getExtraBoilEggPrice());
     }
 
     @Test
     void setExtraBoilEggPrice() {
+        n.setExtraBoilEggPrice(55.8);
+        assertEquals(55.8,n.getExtraBoilEggPrice());
     }
 
     @Test
     void getExtraBamShootPrice() {
+        assertEquals(1,n.getExtraBamShootPrice());
     }
 
     @Test
     void setExtraBamShootPrice() {
+        n.setExtraBamShootPrice(258.6);
+        assertEquals(258.6,n.getExtraBamShootPrice());
     }
 
     @Test
     void getExtraChashuPrice() {
+        assertEquals(2,n.getExtraChashuPrice());
     }
 
     @Test
     void setExtraChashuPrice() {
+        n.setExtraChashuPrice(880.5);
+        assertEquals(880.5,n.getExtraChashuPrice());
     }
 }
