cec33963a10f3ae18a92a68439043f79ae980274
Yuxuan Wu <sodawyx@126.com>
2020-04-02 14:54:18 +0800

    写入Ticket的Controller

---
+++ src/main/controller/GenTicketController.java
@@ -0,0 +1,55 @@
+package main.controller;
+
+import main.entity.Order;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author Yuxuan Wu
+ *
+ */
+public class GenTicketController {
+
+    public static void genTicket(Order order){
+        FileWriter fileWriter = null;
+        File file = new File("/Files/tickets/" + order.getOrderNum()+".txt");
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try{
+            if(file.exists()){
+                file.createNewFile();
+            }
+            fileWriter = new FileWriter(file);
+            fileWriter.write("============TICKET BEGIN==========\n");
+            fileWriter.write("Order Number: "+order.getOrderNum()+"\n");
+            fileWriter.write("Membership Number: "+order.getMemberNum()+"\n");
+            fileWriter.write("Date & Time: "+ df.format(new Date())+"\n");
+            fileWriter.write("Dining Way: "+order.getDiningOption()+"\n");
+            fileWriter.write("----Single Dish----\n");
+            fileWriter.write("Soup: "+order.getSoupType()+"\n");
+            fileWriter.write("Noodles: "+order.getNoodleType()+"\n");
+            fileWriter.write("Spring Onion: "+ order.getSprOnionType()+"\n");
+            fileWriter.write("Nori: "+ order.getNori()+"\n");
+            fileWriter.write("Chashu"+ order.getNori()+ "\n");
+            fileWriter.write("Boiled Egg "+ order.getBoiledEgg()+"\n");
+            fileWriter.write("Spiciness: "+ order.getSpicyIndex()+"\n");
+            fileWriter.write("SINGLE DISH PRICE: "+ order.getFixedPrice()+"\n");
+            fileWriter.write("------Add-ons------\n");
+            fileWriter.write("Extra Nori   Amount: "+order.getExtraNoriNum() +"\n");
+            fileWriter.write("Extra Boiled Egg   Amount: "+order.getExtraBoilEggNum() +"\n");
+            fileWriter.write("Bamboo Shoots   Amount: "+order.getExtraBamshootNum() +"\n");
+            fileWriter.write("Extra Chashu   Amount: "+order.getExtraChashuNum() +"\n");
+            fileWriter.write("ADD-ONS PRICE: "+ order.getAddOnPrice()+"\n");
+            fileWriter.write("-------------------\n");
+            fileWriter.write("TOTAL PRICE: "+ order.getTotalPrice()+"\n");
+            fileWriter.write("=============TICKET END===========\n");
+
+            fileWriter.flush();
+            fileWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
