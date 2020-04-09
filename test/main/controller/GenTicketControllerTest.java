bfe9da244c1b70fcca9266cd06bf2404b2a5b842
yunwujie <1259704385@qq.com>
2020-04-09 20:26:06 +0800

    完成了界面的布局设计以及显示信息测试

---
+++ test/main/controller/GenTicketControllerTest.java
@@ -0,0 +1,10 @@
+package main.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenTicketControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void genTicket() {
+    }
+}
