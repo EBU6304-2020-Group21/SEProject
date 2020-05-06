0d26a042f46ec78f01d393c8be4f7a9fde0dc107
WJZYN <852266201@qq.com>
2020-05-06 22:09:50 +0800

    Menu available

--- src/main/entity/Menu.java
+++ src/main/entity/Menu.java
@@ -18,7 +18,69 @@ public class Menu {
     private boolean isBoilEggAvailable=true;//set available of the Boiled egg
     private boolean isBamShootAvailable=true;//set available of the Bamboo shoot
 
+    public boolean isRamenAvailable() {
+        return isRamenAvailable;
+    }
 
+    public void setRamenAvailable(boolean ramenAvailable) {
+        isRamenAvailable = ramenAvailable;
+    }
+
+    public boolean isTonkotsuAvailable() {
+        return isTonkotsuAvailable;
+    }
+
+    public void setTonkotsuAvailable(boolean tonkotsuAvailable) {
+        isTonkotsuAvailable = tonkotsuAvailable;
+    }
+
+    public boolean isShoyuAvailable() {
+        return isShoyuAvailable;
+    }
+
+    public void setShoyuAvailable(boolean shoyuAvailable) {
+        isShoyuAvailable = shoyuAvailable;
+    }
+
+    public boolean isShioAvailable() {
+        return isShioAvailable;
+    }
+
+    public void setShioAvailable(boolean shioAvailable) {
+        isShioAvailable = shioAvailable;
+    }
+
+    public boolean isNoriAvailable() {
+        return isNoriAvailable;
+    }
+
+    public void setNoriAvailable(boolean noriAvailable) {
+        isNoriAvailable = noriAvailable;
+    }
+
+    public boolean isChashuAvailable() {
+        return isChashuAvailable;
+    }
+
+    public void setChashuAvailable(boolean chashuAvailable) {
+        isChashuAvailable = chashuAvailable;
+    }
+
+    public boolean isBoilEggAvailable() {
+        return isBoilEggAvailable;
+    }
+
+    public void setBoilEggAvailable(boolean boilEggAvailable) {
+        isBoilEggAvailable = boilEggAvailable;
+    }
+
+    public boolean isBamShootAvailable() {
+        return isBamShootAvailable;
+    }
+
+    public void setBamShootAvailable(boolean bamShootAvailable) {
+        isBamShootAvailable = bamShootAvailable;
+    }
 
     public double getFixedPrice() {
         return fixedPrice;
